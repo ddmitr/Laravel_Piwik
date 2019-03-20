a0dde9f83205313cbfea57d541a764a738fee074
Dmitry Borisov <borisov@moqod.com>
2019-03-20 15:22:31 +0300

    Added new methods

--- src/RobBrazier/Piwik/Module/HeatmapSessionRecordingModule.php
+++ src/RobBrazier/Piwik/Module/HeatmapSessionRecordingModule.php
@@ -60,4 +60,81 @@ class HeatmapSessionRecordingModule extends Module
 
         return $this->request->send($options);
     }
+
+    /**
+     * HeatmapSessionRecording.addHeatmap (idSite, name, matchPageRules, sampleLimit = '1000', sampleRate = '5',
+     * excludedElements = '', screenshotUrl = '', breakpointMobile = '', breakpointTablet = '')
+     *
+     * @param array[string]mixed $arguments extra arguments to be passed to the api call
+     * @param string             $format    override format (defaults to one specified in config file)
+     *
+     * @return mixed
+     */
+    public function addHeatmap($arguments = [], $format = null)
+    {
+        $options = $this->getOptions($format)->setArguments($arguments);
+
+        return $this->request->send($options);
+    }
+
+    /**
+     * HeatmapSessionRecording.updateHeatmap (idSite, idSiteHsr, name, matchPageRules, sampleLimit = '1000',
+     * sampleRate = '5', excludedElements = '', screenshotUrl = '', breakpointMobile = '', breakpointTablet = '')
+     *
+     * @param array[string]mixed $arguments extra arguments to be passed to the api call
+     * @param string             $format    override format (defaults to one specified in config file)
+     *
+     * @return mixed
+     */
+    public function updateHeatmap($arguments = [], $format = null)
+    {
+        $options = $this->getOptions($format)->setArguments($arguments);
+
+        return $this->request->send($options);
+    }
+
+    /**
+     * HeatmapSessionRecording.deleteHeatmapScreenshot (idSite, idSiteHsr)
+     *
+     * @param array[string]mixed $arguments extra arguments to be passed to the api call
+     * @param string             $format    override format (defaults to one specified in config file)
+     *
+     * @return mixed
+     */
+    public function deleteHeatmapScreenshot($arguments = [], $format = null)
+    {
+        $options = $this->getOptions($format)->setArguments($arguments);
+
+        return $this->request->send($options);
+    }
+
+    /**
+     * HeatmapSessionRecording.deleteHeatmap (idSite, idSiteHsr)
+     *
+     * @param array[string]mixed $arguments extra arguments to be passed to the api call
+     * @param string             $format    override format (defaults to one specified in config file)
+     *
+     * @return mixed
+     */
+    public function deleteHeatmap($arguments = [], $format = null)
+    {
+        $options = $this->getOptions($format)->setArguments($arguments);
+
+        return $this->request->send($options);
+    }
+
+    /**
+     * HeatmapSessionRecording.endHeatmap (idSite, idSiteHsr)
+     *
+     * @param array[string]mixed $arguments extra arguments to be passed to the api call
+     * @param string             $format    override format (defaults to one specified in config file)
+     *
+     * @return mixed
+     */
+    public function endHeatmap($arguments = [], $format = null)
+    {
+        $options = $this->getOptions($format)->setArguments($arguments);
+
+        return $this->request->send($options);
+    }
 }
